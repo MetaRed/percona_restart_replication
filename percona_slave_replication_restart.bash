def3c88c1644c401d9dd7e1d269bb72a402f5ee0
rlopez <code@meta.red>
2016-09-22 09:30:24 -0400

    Adding email functions.

--- percona_slave_replication_restart.bash
+++ percona_slave_replication_restart.bash
@@ -24,18 +24,23 @@ export MYSQL_USER_PASSWORD
 LOG_DIR=/path/to/script/log/dir
 LOG_FILE=${LOG_DIR}/percona_slave_replication_restart.log
 
+# email function
+notify_email(){
+  mail -s "${0}: failed on ${SERVER_NAME}" $EMAIL
+}
+
 # make sure our log directory exists
 if [ ! -d $LOG_DIR ]; then
   mkdir $LOG_DIR
   if [ ! $? -eq 0 ]; then
-    echo "Unable to create log dir: $LOG_DIR" |mail -s "${0}: failed on $SERVER_NAME" $EMAIL
+    echo "Unable to create log dir: $LOG_DIR" | notify_email
     exit 1
   fi
 else
   touch $LOG_DIR/test
   rm $LOG_DIR/test
   if [ ! $? -eq 0 ]; then
-    echo "Unable to write to log dir: $LOG_DIR" |mail -s "${0}: failed on $SERVER_NAME" $EMAIL
+    echo "Unable to write to log dir: $LOG_DIR" | notify_email
     exit 1
   fi
 fi
@@ -45,7 +50,7 @@ echo "Starting PT-SLAVE-RESTART ${TIMESTAMP}" >>${LOG_FILE}
 # hide slave restart password from process list
 printenv MYSQL_USER_PASSWORD | pt-slave-restart --verbose --user=$MYSQL_USER --ask-pass --monitor --stop --sentinel /tmp/pt-slave-cron --max-sleep 600 --daemonize --log ${LOG_FILE} --skip-count=0 --error-numbers=1205
 if [ ! $? -eq 0 ]; then
-    echo "Unable to run pt-slave-restart" |mail -s "${0}: failed on $SERVER_NAME" $EMAIL
+    echo "Unable to run pt-slave-restart" | notify_email
     exit 1
 fi
 exit 0
